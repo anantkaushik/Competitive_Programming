65c872379bb3e9678e66c88dbfe59e8b8ed180b9
anantkaushik <anant.kaushik2@gmail.com>
2018-05-31 20:00:07 +0530

    print the number of characters in each word of the string

---
+++ Python/count_the_chars.py
@@ -0,0 +1,36 @@
+"""
+Given a string containing multiple words, your task is to count the characters in each word and display them.
+
+Input:
+The first line of input contains an integer T denoting the number of test cases. Then T test cases follow. 
+Each testcase contains a string as input.
+
+Output:
+For each testcase, print the number of characters in each word of the string.
+
+
+Example:
+Input:
+2
+the quick brown fox
+geeks for geeks
+
+Output:
+3 5 5 3
+5 3 5
+
+Explanation:
+For testcase 1:
+the has 3 characters
+quick has 5 characters
+brown has 5 characters
+fox has 3 characters
+"""
+t = int(input())
+while t > 0:
+    arr = list(map(str,input().split()))
+    ans = ""
+    for i in arr:
+        ans +=" " + str(len(i))
+    print(ans[1:])
+    t -= 1
