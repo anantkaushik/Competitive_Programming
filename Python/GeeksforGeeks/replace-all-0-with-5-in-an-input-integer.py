f2cec1c9f9834bc6dadb71f10967d078c0860ddb
anantkaushik <anant.kaushik2@gmail.com>
2018-08-29 23:34:53 +0530

    from GeeksforGeeks

---
+++ Python/GeeksforGeeks/replace-all-0-with-5-in-an-input-integer.py
@@ -0,0 +1,32 @@
+"""
+Given a number your task is to complete the function convertFive which replace all zeros in the number with 5 and returns the number.
+
+Input:
+The first line of input contains an integer T denoting the number of test cases . Then T test cases follow . Each test case contains a 
+single integer N denoting the number.
+
+Output:
+The output of the function will be an integer where all zero's are converted to 5 .
+
+Constraints:
+1<=T<100
+1<=N<=10000
+
+Example:
+Input
+2
+1004
+121
+
+Ouput
+1554
+121
+"""
+if __name__=='__main__':
+    t = int(input())
+    for i in range(t):
+        n=int(input())
+        print(convertFive(n))
+
+def convertFive(n):
+    return str(n).replace("0","5")
