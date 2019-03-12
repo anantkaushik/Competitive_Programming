22b22a5c7376adeff22ec10844fbcb050268f734
anantkaushik <anant.kaushik2@gmail.com>
2019-03-12 22:17:48 +0530

    Trailing zeroes in factorial

---
+++ Python/GeeksforGeeks/trailing-zeroes-in-factorial.py
@@ -0,0 +1,31 @@
+"""
+Problem Link: https://practice.geeksforgeeks.org/problems/trailing-zeroes-in-factorial/0
+
+For an integer n find number of trailing zeroes in n! . 
+
+Input:
+The first line contains an integer 'T' denoting the total number of test cases. 
+In each test cases, it contains an integer 'N'.
+
+Output:
+In each seperate line output the answer to the problem.
+
+Constraints:
+1<=T<=100
+1<=N<=1000
+
+Example:
+Input:
+1
+9
+
+Output:
+1
+"""
+for _ in range(int(input())):
+    n = int(input())
+    count = 0
+    while n:
+        n //= 5
+        count += n
+    print(count)
