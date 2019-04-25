e7d02f82097d81ad99e9b459f99cbc97c2638615
anantkaushik <anant.kaushik2@gmail.com>
2019-04-25 23:51:57 +0530

    Comment in Python

---
+++ Python/GeeksforGeeks/comment-in-python.py
@@ -0,0 +1,39 @@
+"""
+Given two print statement, the task is to comment the first one, leave next as it is and comment next three (multiple line comment) 
+such that only required output will be generated.
+
+Hint:
+#: is used to comment a single line.
+''' ''' : is used to comment multiple lines.
+
+Input:
+None
+
+Output:
+Geeks For Geeks
+
+Example:
+After commenting the required line, Geeks For Geeks is printed, which is required output.
+"""
+#Initial Template for Python 3
+# Position this line where user code will be pasted.
+#Python code to use comment
+# Driver Code
+def main():
+    
+    print_func()
+        
+if __name__ == '__main__':
+    main()
+
+''' Please note that it's Function problem i.e.
+you need to write your solution in the form of Function(s) only.
+Driver Code to call/invoke your function is mentioned above. '''
+
+#User function Template for python3
+def print_func():
+    # print ("Geeks Classes")
+    print ("Geeks For Geeks")
+    """print ("Geeks Quiz")
+    print ("Geeks")
+    print ("Geeks For")"""
