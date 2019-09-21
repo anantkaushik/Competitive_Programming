0277ac06a7bb4c6928dcd2dc742fb399aeb9be14
Anant Kaushik <anant.kaushik2@gmail.com>
2019-09-21 23:11:02 +0530

    Delete without head pointer

--- Python/GeeksforGeeks/delete-without-head-pointer.py
+++ Python/GeeksforGeeks/delete-without-head-pointer.py
@@ -33,6 +33,85 @@ Output:
 Explanation:
 Testcase 1: After deleting 20 from the linked list, we have remaining nodes as 10, 4 and 30.
 """
-def deleteNode(node):
-    node.val = node.next.val
-    node.next = node.next.next
+import atexit
+import io
+import sys
+_INPUT_LINES = sys.stdin.read().splitlines()
+input = iter(_INPUT_LINES).__next__
+_OUTPUT_BUFFER = io.StringIO()
+sys.stdout = _OUTPUT_BUFFER
+@atexit.register
+def write():
+    sys.__stdout__.write(_OUTPUT_BUFFER.getvalue())
+    
+# Node Class
+class Node:
+    def __init__(self, data):   # data -> value stored in node
+        self.data = data
+        self.next = None
+# Linked List Class
+class LinkedList:
+    def __init__(self):
+        self.head = None
+    # creates a new node with given value and appends it at the end of the linked list
+    def append(self, new_value):
+        new_node = Node(new_value)
+        if self.head is None:
+            self.head = new_node
+            return
+        curr_node = self.head
+        while curr_node.next is not None:
+            curr_node = curr_node.next
+        curr_node.next = new_node
+    def getNode(self,value): # return node with given value, if not present return None
+        curr_node=self.head
+        while(curr_node.next and curr_node.data != value):
+            curr_node=curr_node.next
+        if(curr_node.data==value):
+            return curr_node
+        else:
+            return None
+    # prints the elements of linked list starting with head
+    def printList(self):
+        if self.head is None:
+            print(' ')
+            return
+        curr_node = self.head
+        while curr_node:
+            print(curr_node.data,end=" ")
+            curr_node=curr_node.next
+        print(' ')
+if __name__ == '__main__':
+    t=int(input())
+    for cases in range(t):
+        n = int(input())
+        a = LinkedList() # create a new linked list 'a'.
+        nodes = list(map(int, input().strip().split()))
+        for x in nodes:
+            a.append(x)
+        del_elem = int(input())
+        to_delete=a.getNode(del_elem)
+        deleteNode(to_delete)
+        a.printList()
+''' This is a function problem.You only need to complete the function given below '''
+#User function Template for python3
+'''
+    Your task is to delete the given node from
+	the linked list, without using head pointer.
+	
+	Function Arguments: node (given node to be deleted) 
+	Return Type: None, just delete the given node from the linked list.
+	{
+		# Node Class
+		class Node:
+		    def __init__(self, data):   # data -> value stored in node
+		        self.data = data
+		        self.next = None
+	}
+'''
+def deleteNode(curr_node):
+    #code here
+    if not curr_node:
+        return None
+    curr_node.data = curr_node.next.data
+    curr_node.next = curr_node.next.next
