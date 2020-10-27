f4d98d3996b4fbb960179a835e0e4d6b1c709db9
Rishabh Sethi <54479777+i-am-expert@users.noreply.github.com>
2020-10-27 23:03:25 +0530

    Added different approach

--- C/gcd.c
+++ C/gcd.c
@@ -30,3 +30,23 @@ int main(){
     }
 	return 0;
 }
+
+
+/* Different Approach */
+int gcd(int a, int b)
+{
+    // Everything divides 0 
+    if (a == 0)
+       return b;
+    if (b == 0)
+       return a;
+  
+    // base case
+    if (a == b)
+        return a;
+  
+    // a is greater
+    if (a > b)
+        return gcd(a-b, b);
+    return gcd(a, b-a);
+}
