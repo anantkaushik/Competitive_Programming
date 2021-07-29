b4cc6a870d4ca15f668c9a11dc1a56d3803b1b8f
anant <anant.kaushik2@gmail.com>
2021-07-29 21:05:47 +0530

    Query Params Parser
    Add another approach to solve the problem using "reduce".

--- Clojure/other/query_params_parser.clj
+++ Clojure/other/query_params_parser.clj
@@ -3,6 +3,7 @@
 
 (require '[clojure.string :as s])
 
+;Method 1
 (defn convert-key-value
     [key-value]
     (vector (keyword (first key-value)) (second key-value))
@@ -12,4 +13,22 @@
     [params-str]
     (into {} (map #(convert-key-value (s/split % #"=")) (s/split params-str #"&")))
 )
+(println (generate-params-map "param1=v1&param2=v2&param3=v3"))
+
+
+;Method 2
+(defn generate-params-map
+      "Function to convert query param string to map"
+      [params-str]
+      (reduce (fn [params-map param-key-val]
+                  (conj params-map
+                        (#((fn [key-value]
+                               (hash-map (keyword (first key-value)) (second key-value)))
+                           (s/split % #"="))
+                            param-key-val)))
+              {}
+              (s/split params-str #"&")
+              )
+      )
+
 (println (generate-params-map "param1=v1&param2=v2&param3=v3"))
