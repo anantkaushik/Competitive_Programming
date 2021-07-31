85c67b6b753a87cc7e1f7dd56d95fc7f4caed693
anant <anant.kaushik2@gmail.com>
2021-07-31 22:48:27 +0530

    Fizzbuzz

--- Clojure/hackerearth/fizzbuzz.clj
+++ Clojure/hackerearth/fizzbuzz.clj
@@ -1,25 +1,25 @@
-;Problem Link: https://www.hackerearth.com/problem/algorithm/fizzbuzz/
-;
-;Write a program that prints the numbers from 1 to 100. But for multiples of three print “Fizz” instead of the number
-;and for the multiples of five print “Buzz”. For numbers which are multiples of both three and five print “FizzBuzz”.
-;Print a new line after each string or number.
+;; Problem Link: https://www.hackerearth.com/problem/algorithm/fizzbuzz/
+;; Write a program that prints the numbers from 1 to 100. But for
+;; multiples of three print “Fizz” instead of the number
+;; and for the multiples of five print “Buzz”. For numbers which are
+;; multiples of both three and five print “FizzBuzz”.
+;; Print a new line after each string or number.
 
 (defn fizz-buzz
-      [number]
-      (map
-        (fn [n]
-            (let [result
-                  (str
-                    (if (zero? (mod n 3))
-                      "Fizz")
-                    (if (zero? (mod n 5))
-                      "Buzz"))]
-                 (if (empty? result)
-                   (println n)
-                   (println result))
-                 ))
-        (range 1 (inc number)))
-      )
+  [number]
+  (map
+    (fn [n]
+      (let [result
+            (str
+              (if (zero? (mod n 3))
+                "Fizz")
+              (if (zero? (mod n 5))
+                "Buzz"))]
+        (println (if (empty? result)
+                   n
+                   result))))
+    (range 1 (inc number))))
+
+
 (def x (Integer/parseInt (read-line)))
-(println x)
 (fizz-buzz x)
